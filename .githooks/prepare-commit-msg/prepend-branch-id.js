10ed4318297b754c395cc6ce28eee769808ad59d
FinlayS <fsgm01@gmail.com>
2020-04-04 10:25:17 +0100

    ABC-123 not adding dotenv ABC-123

--- .githooks/prepare-commit-msg/prepend-branch-id.js
+++ .githooks/prepare-commit-msg/prepend-branch-id.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
+require("dotenv");
+const exec = require('child_process').exec;
+const util = require('util');
+const fs = require('fs');
 
-const exec = require('child_process').exec
-const util = require('util')
-const fs = require('fs')
-const utils = require('../utils')
-let contents = null
+const utils = require('../utils');
 
-const commitFile = process.env.GIT_PARAMS;
+let contents = null;
 
+const commitFile = process.env.HUSKY_GIT_PARAMS;
 
 // expect .git/COMMIT_EDITMSG
-console.log('bob-before', commitFile)
+console.log('bob-before', commitFile);
 if (/COMMIT_EDITMSG/g.test(commitFile)) {
-  console.log('bob-after')
+  console.log('bob-after');
   // look for current branch name
   exec('git branch | grep \'*\'',
     function (err, stdout) {
@@ -22,13 +23,13 @@ if (/COMMIT_EDITMSG/g.test(commitFile)) {
         process.exit(0)
       }
       // opens .git/COMMIT_EDITMSG.
-      contents = fs.readFileSync(commitFile)
+      contents = fs.readFileSync(commitFile);
 
       // trims extra characters from start/end of line
-      let name = stdout.replace('* ', '').replace('\n', '')
+      let name = stdout.replace('* ', '').replace('\n', '');
 
       // On the release branch: no need for Jira ticket ID
-      if (name.includes('release')) process.exit(0)
+      if (name.includes('release')) process.exit(0);
 
       // If the branch has a description, pull that
       exec('git config branch.' + name + '.description',
@@ -43,18 +44,19 @@ if (/COMMIT_EDITMSG/g.test(commitFile)) {
           // '(no branch)' indicates we are in a rebase or other non-HEAD scenario
           if (name !== '(no branch)') {
             const branchId = utils.getBranchId(name)
-            const contentsStartWithValidBranchId = utils.startsWithValidBranchId(contents.toString())
+            const contentsStartWithValidBranchId = utils.startsWithValidBranchId(contents.toString());
             if (!branchId && !contentsStartWithValidBranchId) {
-              process.stdout.write(`COMMIT FAILED: No {JIRA_ID} or HOTFIX found in branch name or at the beginning of your commit message.\n`)
-              process.exitCode = 1
+              process.stdout.write(`COMMIT FAILED: No {JIRA_ID} or HOTFIX found in branch name or at the beginning of your commit message.\n`);
+              process.exitCode = 1;
               process.exit()
             }
 
             // Prepend ID to original contents.
-            if (!contentsStartWithValidBranchId) contents = util.format(`${branchId.toUpperCase()} ${contents}\n`)
+            if (!contentsStartWithValidBranchId) contents = util.format(`${branchId.toUpperCase()} ${contents}\n`);
 
             // write contents back out to .git/COMMIT_EDITMSG
-            fs.writeFileSync(commitFile, contents)
+            fs.writeFileSync(commitFile, contents);
+            console.log(contents);
             process.exit(0)
           } else {
             process.exit(0)
