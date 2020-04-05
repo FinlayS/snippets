2e98e8c771d61d5d075bc89340b5a186abce17b8
FinlayS <fsgm01@gmail.com>
2020-04-05 18:41:53 +0100

    ABC-999 more2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ["plugin:react/recommended", "standard"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
     "no-unused-vars": [
       "error",
@@ -28,8 +23,11 @@ module.exports = {
         vars: "all",
         args: "after-used",
         ignoreRestSiblings: false,
-        argsIgnorePattern: "^_"
-      }
-    ]
+        argsIgnorePattern: "^_",
+      },
+    ],
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    indent: ["error", 4],
   },
-}
+};
