0205f89177cb61ba6f79f91296f4adde76e94edc
FinlayS <fsgm01@gmail.com>
2020-04-10 06:44:21 +0100

    XYZ-321 removing old npm check

--- scripts/npm-version-check.sh
+++
@@ -1,8 +0,0 @@
-NPM_VERSION='6.13.4'
-
-if ( $(npm -v) != $NPM_VERSION ) ; then
-    VERS=$(node -v)
-    echo "your version is ${VERS}"
-  echo "${RED}Please install correct version of npm by running${SET} ${YELLOW}'npm i npm@${NPM_VERSION} -g'${SET}"
-  exit 1
-fi
