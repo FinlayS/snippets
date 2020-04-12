7c46aebb79212aadecdad0f756a6e25826fbb891
Finlay <FinlayS@users.noreply.github.com>
2020-04-12 16:57:11 +0100

    Abc 999 pre push pretty (#2)
    * ABC-999 test
    
    * ABC-999 test again
    
    * ABC-999 more
    
    * ABC-999 more2
    
    * ABC-999 more2
    
    * ABC-999 more3
    
    * ABC-999 more4
    
    * ABC-999 more4
    
    * updating lint staged

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+        argsIgnorePattern: "^_",
+      },
+    ],
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    indent: ["error", 4],
+  },
+};
