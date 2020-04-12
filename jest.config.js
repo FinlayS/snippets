e897be19d5149980ca14f2869d305d47e1f46587
FinlayS <fsgm01@gmail.com>
2020-04-12 10:06:04 +0100

    ABC-123 seting up test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageReporters: ["json", "text"],
+  moduleDirectories: ["node_modules", "."],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
